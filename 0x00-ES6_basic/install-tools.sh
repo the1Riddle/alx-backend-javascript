490b5db60d67469f550c6ca8c87de50b1fc61e36
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-05 22:50:54 +0300

    install-tools.sh

--- 0x00-ES6_basic/install-tools.sh
+++ 0x00-ES6_basic/install-tools.sh
@@ -1,36 +1,86 @@
 #!/bin/bash
 
-# Create a new Node.js project (skip if you already have a package.json file)
-npm init -y
+# Install NodeJS 12.11.x
 
-# Install Jest and its dependencies
-npm install --save-dev jest
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
 
-# Install Babel and its dependencies
-npm install --save-dev @babel/core @babel/preset-env @babel/cli
-
-# Install ESLint and its dependencies
-npm install --save-dev eslint
-
-# Create a basic Babel configuration file
-echo '{
-  "presets": ["@babel/preset-env"]
-}' > .babelrc
-
-# Create a basic ESLint configuration file
-npx eslint --init
-
-# Optionally, you can add a script to your package.json file for running tests with Jest
-echo '{
+# Configuration files
+cat <<EOF > package.json
+{
   "scripts": {
-    "test": "jest"
+    "lint": "./node_modules/.bin/eslint",
+    "check-lint": "lint [0-9]*.js",
+    "dev": "npx babel-node",
+    "test": "jest",
+    "full-test": "./node_modules/.bin/eslint [0-9]*.js && jest"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.6.0",
+    "@babel/node": "^7.8.0",
+    "@babel/preset-env": "^7.6.0",
+    "eslint": "^6.4.0",
+    "eslint-config-airbnb-base": "^14.0.0",
+    "eslint-plugin-import": "^2.18.2",
+    "eslint-plugin-jest": "^22.17.0",
+    "jest": "^24.9.0"
   }
-}' > package.json
+}
+EOF
 
-# Display installation completion message
-echo "Jest, Babel, and ESLint have been installed successfully!"
+cat <<EOF > babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+EOF
 
-# Provide instructions for running tests (if Jest is installed)
-echo "To run tests, use the following command:"
-echo "npm test"
+cat <<EOF > .eslintrc.js
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
+EOF
+
+npm install
 
