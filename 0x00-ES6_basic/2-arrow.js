e82f582e176d51a6c2150c071ae156c3feb9fcc6
the1Riddle <elvisotienomboya@gmail.com>
2024-03-06 15:09:04 +0000

    task two

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
