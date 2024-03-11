130b9044172ba24736ced0e20bb33e5c07da2279
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-11 14:04:17 +0300

    0x02. ES6 classes  (#3)
    compleated 0x02. ES6 classes  project tasks.
    
    ---------
    
    Co-authored-by: the-1Riddle <elv.s.earl@gmail.com>

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
+/** some comments **/
+
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+/** `Pricing` class implementation **/
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // amount getter
+  get amount() {
+    return this._amount;
+  }
+
+  // amount setter
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // currency getter
+  get currency() {
+    return this._currency;
+  }
+
+  // currency setter
+  set currency(cur) {
+    this._currency = cur;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  /** Convert the price **/
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
