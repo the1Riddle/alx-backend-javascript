ee84959a33ece8e9d241da38eb8bb126496add54
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-04-08 17:43:13 +0300

    Working (#9)
    * initial commit for the project
    
    * Update README.md
    
    Co-authored-by: the-1Riddle <elv.s.earl@gmail.com>
    
    * updates
    
    ---------
    
    Co-authored-by: the-1Riddle <elv.s.earl@gmail.com>

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,9 @@
 export default function returnHowManyArguments(...args) {
-  let count = 0;
+        let count = 0;
 
 
-  for (const _ of args) {
-    count += 1;
-  }
-  return count;
+        for (const _ of args) {
+                count += 1;
+        }
+        return count;
 }
